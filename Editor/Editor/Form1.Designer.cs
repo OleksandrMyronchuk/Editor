ad070c9edf6af5d38d89fd0d2d235695ccf65703
OleksandrMyronchuk <hy677ba@yandex.ua>
2016-12-01 17:25:40 +0200

    Add files via upload

---
+++ Editor/Editor/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.review = new System.Windows.Forms.Button();
+            this.pathToFile = new System.Windows.Forms.Label();
+            this.path = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(528, 319);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.save.Location = new System.Drawing.Point(384, 357);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 4;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel.Location = new System.Drawing.Point(465, 357);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 5;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // review
+            // 
+            this.review.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.review.Location = new System.Drawing.Point(465, 4);
+            this.review.Name = "review";
+            this.review.Size = new System.Drawing.Size(75, 23);
+            this.review.TabIndex = 10;
+            this.review.Text = "Review";
+            this.review.UseVisualStyleBackColor = true;
+            this.review.Click += new System.EventHandler(this.review_Click_1);
+            // 
+            // pathToFile
+            // 
+            this.pathToFile.Location = new System.Drawing.Point(12, 9);
+            this.pathToFile.Name = "pathToFile";
+            this.pathToFile.Size = new System.Drawing.Size(63, 13);
+            this.pathToFile.TabIndex = 11;
+            this.pathToFile.Text = "Path to file";
+            // 
+            // path
+            // 
+            this.path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.path.Location = new System.Drawing.Point(72, 9);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(387, 13);
+            this.path.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 392);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.review);
+            this.Controls.Add(this.pathToFile);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.save);
+            this.Name = "Form1";
+            this.Text = "Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button review;
+        private System.Windows.Forms.Label pathToFile;
+        private System.Windows.Forms.Label path;
+    }
+}
+
---
+++ Editor/Editor/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    public partial class Form1 : Form
+    {
+
+        public string currentPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void review_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            OpenFileDialog OPF1 = new OpenFileDialog();
+            OPF1.ShowDialog();
+            currentPath = OPF1.FileName;
+
+            if (!File.Exists(currentPath))
+                return;
+
+            path.Text = currentPath;
+
+            using (FileStream fs = File.Open(currentPath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    richTextBox1.Text += temp.GetString(b);
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (currentPath == null || currentPath == "")
+                return;
+
+            using (FileStream fs = File.Open(currentPath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes( richTextBox1.Text );
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
